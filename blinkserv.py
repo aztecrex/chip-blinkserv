69ec137dcbb80af094930a0458c217d8f7cc11e1
Greg Wiley <aztec.rex@jammm.com>
2017-08-29 05:20:32 +0000

    light control

--- blinkserv.py
+++ blinkserv.py
@@ -1,7 +1,22 @@
+import CHIP_IO.GPIO as GPIO
 from flask import Flask
 app = Flask(__name__)
+
+GPIO.setup("XIO-P0",GPIO.OUT)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/on")
+def on():
+    GPIO.output("XIO-P0",GPIO.LOW)
+    return ""
+
+@app.route("/off")
+def off():
+    GPIO.output("XIO-P0",GPIO.HIGH)
+    return ""
+
+
 
