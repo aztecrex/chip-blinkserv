3dfad49dac5715d1723ec01da452da591a86e90c
Greg Wiley <aztec.rex@jammm.com>
2017-08-30 03:55:04 +0000

    ajax for led

--- blinkserv.py
+++ blinkserv.py
@@ -1,30 +1,53 @@
 import CHIP_IO.GPIO as GPIO
 from flask import Flask
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, Response
 
 app = Flask(__name__)
 
 GPIO.setup("XIO-P0",GPIO.OUT)
 GPIO.output("XIO-P0",GPIO.HIGH)
 
+def ledTurnOn():
+    GPIO.output("XIO-P0",GPIO.LOW)
+
+def ledTurnOff():
+    GPIO.output("XIO-P0",GPIO.HIGH)
+
+def ledIsOn():
+    return not GPIO.input("XIO-P0")
+    
 @app.route("/")
 def index():
     return render_template('index.html')
 
 @app.route("/led")
 def led():
-    if GPIO.input("XIO-P0"):
-        return render_template('display.html', status="off")
-    else:
-        return render_template('display.html', status="on")
+    return render_template('led.html')
 
 @app.route("/on")
 def on():
-    GPIO.output("XIO-P0",GPIO.LOW)
+    ledTurnOn()
     return redirect(url_for('led'))
 
 @app.route("/off")
 def off():
-    GPIO.output("XIO-P0",GPIO.HIGH)
+    ledTurnOff()
     return redirect(url_for('led'))
+
+@app.route("/api/led")
+def apiLedStatus():
+    rep = render_template('led.json', status=ledIsOn())
+    return Response(rep, mimetype='application/json')
+
+@app.route("/api/led/on", methods=['POST'])
+def apiLedOn():
+    ledTurnOn()
+    rep = render_template('led.json', status=True)
+    return Response(rep, mimetype='application/json')
+
+@app.route("/api/led/off", methods=['POST'])
+def apiLedOff():
+    ledTurnOff()
+    rep = render_template('led.json', status=False)
+    return Response(rep, mimetype='application/json')
 
