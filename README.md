1c543433ea19dce47b9ebac3a57eeb76e8c08ca1
Greg Wiley <aztec.rex@jammm.com>
2017-08-30 03:05:21 +0000

    control LED

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 Demonstrate a web server running on a C.H.I.P. and
 controlling its devices.
 
+## Demos
 
+### LED - turn led on and off
+
+Prototype that uses simple page redirects to display its home.
 
 
--- blinkserv.py
+++ blinkserv.py
@@ -1,22 +1,30 @@
 import CHIP_IO.GPIO as GPIO
 from flask import Flask
+from flask import render_template, redirect, url_for
+
 app = Flask(__name__)
 
 GPIO.setup("XIO-P0",GPIO.OUT)
+GPIO.output("XIO-P0",GPIO.HIGH)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
+
+@app.route("/led")
+def led():
+    if GPIO.input("XIO-P0"):
+        return render_template('display.html', status="off")
+    else:
+        return render_template('display.html', status="on")
 
 @app.route("/on")
 def on():
     GPIO.output("XIO-P0",GPIO.LOW)
-    return ""
+    return redirect(url_for('led'))
 
 @app.route("/off")
 def off():
     GPIO.output("XIO-P0",GPIO.HIGH)
-    return ""
-
-
+    return redirect(url_for('led'))
 
